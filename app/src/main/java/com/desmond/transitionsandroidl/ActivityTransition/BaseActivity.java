f8187f7cf70b3a632c98581353d9da09d8720cfe
boxme <desmond.ng.yang.yi@gmail.com>
2015-08-20 14:53:24 +0800

    Delayed transitions in activity

--- app/src/main/java/com/desmond/transitionsandroidl/ActivityTransition/BaseActivity.java
+++ app/src/main/java/com/desmond/transitionsandroidl/ActivityTransition/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.desmond.transitionsandroidl.ActivityTransition;
 
-import android.support.v4.app.ActivityCompat;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -8,14 +9,28 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.desmond.transitionsandroidl.R;
+import com.desmond.transitionsandroidl.TransitionHelper;
 
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements TransitionHelper.Source {
 
     Toolbar mToolBar;
+    TransitionHelper mTransitionHelper;
 
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mTransitionHelper.onResume();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TransitionHelper.init(this, savedInstanceState);
     }
 
     @Override
@@ -47,7 +62,17 @@ public class BaseActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        ActivityCompat.finishAfterTransition(this);
+        mTransitionHelper.onBackPressed();
         super.onBackPressed();
+    }
+
+    @Override
+    public TransitionHelper getTransitionHelper() {
+        return mTransitionHelper;
+    }
+
+    @Override
+    public void setTransitionHelper(TransitionHelper transitionHelper) {
+        mTransitionHelper = transitionHelper;
     }
 }
--- app/src/main/java/com/desmond/transitionsandroidl/TransitionHelper.java
+++ app/src/main/java/com/desmond/transitionsandroidl/TransitionHelper.java
@@ -1,13 +1,17 @@
 package com.desmond.transitionsandroidl;
 
 import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.view.Window;
 
 import com.desmond.transitionsandroidl.ActivityTransition.BaseActivity;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -15,6 +19,26 @@ import java.util.Arrays;
  * Created by desmond on 20/8/15.
  */
 public class TransitionHelper {
+
+    WeakReference<BaseActivity> mActivityRef;
+
+    boolean mIsAfterEnter;
+    boolean mIsPostponeEnterTransition;
+
+    public interface Source {
+        TransitionHelper getTransitionHelper();
+        void setTransitionHelper(TransitionHelper transitionHelper);
+    }
+
+    private TransitionHelper(BaseActivity activity, Bundle savedInstanceState) {
+        mActivityRef = new WeakReference<>(activity);
+        mIsAfterEnter = savedInstanceState != null;
+        postponeEnterTransition();
+    }
+
+    public static void init(Source source, Bundle savedInstanceState) {
+        source.setTransitionHelper(new TransitionHelper((BaseActivity) source, savedInstanceState));
+    }
 
     public static ActivityOptionsCompat makeOptionsCompat(
             BaseActivity activity, Pair<View, String>... shareElements ) {
@@ -32,5 +56,51 @@ public class TransitionHelper {
         shareElements = list.toArray(new Pair[list.size()]);
 
         return ActivityOptionsCompat.makeSceneTransitionAnimation(activity, shareElements);
+    }
+
+    private void postponeEnterTransition() {
+        BaseActivity activity = mActivityRef.get();
+        if (mIsAfterEnter || activity == null) return;
+
+        mIsPostponeEnterTransition = true;
+        ActivityCompat.postponeEnterTransition(activity);
+    }
+
+    private void startPostponedEnterTransition() {
+        final BaseActivity activity = mActivityRef.get();
+        if (activity == null) return;
+
+        final View decor = activity.getWindow().getDecorView();
+        decor.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                decor.getViewTreeObserver().removeOnPreDrawListener(this);
+                ActivityCompat.startPostponedEnterTransition(activity);
+                return true;
+            }
+        });
+    }
+
+    /**
+     * Should be called from Activity.onResume
+     */
+    public void onResume() {
+        if (mIsAfterEnter) return;
+
+        onViewCreated();
+    }
+
+    public void onViewCreated() {
+        if (mIsPostponeEnterTransition) {
+            mIsPostponeEnterTransition = false;
+            startPostponedEnterTransition();
+        }
+    }
+
+    public void onBackPressed() {
+        BaseActivity activity = mActivityRef.get();
+        if (activity == null) return;
+
+        ActivityCompat.finishAfterTransition(activity);
     }
 }
